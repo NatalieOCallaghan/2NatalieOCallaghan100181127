5db053f93db7f828d95231b1948253ec97a17b29
NatalieOCallaghan <100181127@student.burnley.ac.uk>
2023-10-01 20:05:16 +0100

    Week 2 Exercise 4
    Exercise to print the seven times multiplication table.

---
+++ src/Week2/exercise 4.java
@@ -0,0 +1,11 @@
+package Week2;
+
+class SevenTimesTable{
+    public static void main(String args[]){
+        // for loop with a count from 1-12.
+        for(int count=1; count<13; count++){
+        // Takes the count value and uses it to calculate the current multiplication
+        System.out.println(count+" x 7 = "+((count)*7));
+        }
+    }
+}
