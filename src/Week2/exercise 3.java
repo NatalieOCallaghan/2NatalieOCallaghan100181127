1c768bc47b97c8006b8eea1cf05cccf425286871
NatalieOCallaghan <100181127@student.burnley.ac.uk>
2023-10-01 15:55:32 +0100

    Week 2 Exercise 3
    Exercise to output digits between the range 0 to 9 using a loop.

---
+++ src/Week2/exercise 3.java
@@ -0,0 +1,10 @@
+package Week2;
+
+class DigitOutput{
+    public static void main(String args[]){
+        // for loop that outputs numbers until it reaches 9.
+        for(int count=0; count<8; count++){
+        System.out.println((count+1));
+        }
+    }
+}
