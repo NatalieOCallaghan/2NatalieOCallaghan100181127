da5d8a8e4bf9bd90770fc5284a5a4429b32eb434
NatalieOCallaghan <100181127@student.burnley.ac.uk>
2023-10-13 15:24:31 +0100

    Extra Structure for CW1
    More Planning/Structure Work for CW1

--- src/CW1/Songlist.java
+++ src/CW1/Songlist.java
@@ -1,20 +1,24 @@
 package CW1;
+import java.util.ArrayList;
 
 public class Songlist {
     
    // Will be an arraylist of Songs.
-   
+   private ArrayList<Song> songlist = new ArrayList<Song>();
 
    // Populate songlist
-
+   
 
    // Add a new song to the songlist, 
-   
+   public void addSongToSonglist(){
+
+   }
 
 
    // Remove a song from the songlist, 
-   
-
+    public void removeSongFromSonglist(){
+    
+   }
 
    // Print the songlist
    public void printSonglist(){
