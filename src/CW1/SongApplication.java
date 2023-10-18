a581f409e9ef7075be290d256054cb17211117fc
NatalieOCallaghan <100181127@student.burnley.ac.uk>
2023-10-18 22:14:33 +0100

    CW1 Adding simple themes
    Simple themes have been set up through the assistance of the ConsoleColours helper. The user can decide what colour they want the program to output (singular colours).

--- src/CW1/SongApplication.java
+++ src/CW1/SongApplication.java
@@ -1,5 +1,7 @@
 package CW1;
-// Imports InputReader
+// Imports ConsoleColours (Used for themes)
+import helpers.ConsoleColours;
+// Imports InputReader (Used for user inputs)
 import helpers.InputReader;
 
 
@@ -32,9 +34,36 @@ public class SongApplication{
         songlist.addSongToSonglist(song9);
         songlist.addSongToSonglist(song10);
 
+        // Defaults the theme of the application to "none"
+        System.out.println(ConsoleColours.ANSI_RESET);
+        String theme="none";
         // Defaults the menu choice to 0, as a value is needed for the "do while" loop to start
         int menuChoice=0;
         do {
+            // Conditional statements to check and set the theme of the program. It will be none as default but then will change if the user decides to change it
+            // This is checked with every loop to ensure that what the user is seeing is up to date with the theme they want
+            if (theme.equalsIgnoreCase("Black")) {
+                // Makes use of the "ConsoleColours" helper to set the theme to the correct colour
+                System.out.println(ConsoleColours.ANSI_BLACK);
+            } else if (theme.equalsIgnoreCase("White")) {
+                System.out.println(ConsoleColours.ANSI_WHITE);
+            } else if (theme.equalsIgnoreCase("Red")) {
+                System.out.println(ConsoleColours.ANSI_RED);
+            } else if (theme.equalsIgnoreCase("Green")) {
+                System.out.println(ConsoleColours.ANSI_GREEN);
+            } else if (theme.equalsIgnoreCase("Yellow")) {
+                System.out.println(ConsoleColours.ANSI_YELLOW);
+            } else if (theme.equalsIgnoreCase("Blue")) {
+                System.out.println(ConsoleColours.ANSI_BLUE);
+            } else if (theme.equalsIgnoreCase("Purple")) {
+                System.out.println(ConsoleColours.ANSI_PURPLE);
+            } else if (theme.equalsIgnoreCase("Cyan")) {
+                System.out.println(ConsoleColours.ANSI_CYAN);       
+            }
+            else {
+                // If the theme is none then it outputs the default colour (or performs a "reset" on the theme)
+                System.out.println(ConsoleColours.ANSI_RESET);
+            }
             
             // Menu options
             System.out.println("");
@@ -44,7 +73,8 @@ public class SongApplication{
             System.out.println("2. Remove a song from the songlist");
             System.out.println("3. Print current songlist");
             System.out.println("4. Filter songs that have over an amount of playcounts");
-            System.out.println("5. Quit application");
+            System.out.println("5. Change theme");
+            System.out.println("6. Quit application");
             System.out.println("");
 
             // User enters their choice
@@ -89,9 +119,30 @@ public class SongApplication{
                     int playCountFilter=InputReader.getInt("Please enter an amount: ");
                     songlist.songsOverGivenPlayCount(playCountFilter);
                     break;
-    
-                // Informs the user that the program is stopping (the "do while" loop will stop looping)
+                
                 case 5:
+                    System.out.println("Choice 5");
+                    // Outputs the options to the user and defaults validation to "false"
+                    System.out.println("Themes: Black, White, Red, Green, Yellow, Blue, Purple, Cyan");
+                    Boolean themeValid=false;
+                    // "Do while" loop to ensure that the user selects a valid theme before the program continues
+                    do { 
+                        // User inputs the theme they would like to use
+                        theme = InputReader.getString("What theme would you like to choose?: ");
+                        // If the theme entered equals any of the available themes, the validator, themeValid, becomes true
+                        if (theme.equalsIgnoreCase("Black") || theme.equalsIgnoreCase("White") || theme.equalsIgnoreCase("Red") || theme.equalsIgnoreCase("Green") || theme.equalsIgnoreCase("Yellow") || theme.equalsIgnoreCase("Blue") || theme.equalsIgnoreCase("Purple") || theme.equalsIgnoreCase("Cyan")) {
+                            themeValid=true;
+                        // If the theme entered doesn't equal any of the available themes, it will output an error message and keep the validator false
+                        } else {
+                            System.out.println("Theme invalid. Please try again.");
+                            themeValid=false;
+                        }
+                    // Will continue to loop until the theme entered is valid
+                    } while (themeValid == false);
+                    break;
+
+                // Informs the user that the program is stopping (the "do while" loop will stop looping)
+                case 6:
                     System.out.println("Exiting application...");
                     break;
     
@@ -99,7 +150,7 @@ public class SongApplication{
                 default:
                     System.out.println("Please select a valid choice (1/2/3/4/5)");
             }
-        // Ensures that the program will repeat until option 5 (or "Quit application") is selected
-        } while(menuChoice != 5);
+        // Ensures that the program will repeat until option 6 (or "Quit application") is selected
+        } while(menuChoice != 6);
     }
 }
